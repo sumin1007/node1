dc1012d00b8235b029dee74d748d8b067c336a80
suminkim1007 <suminmusic99@gmail.com>
2020-04-07 11:08:25 +0900

    practice

--- chap02/concat.js
+++ chap02/concat.js
@@ -4,3 +4,9 @@ let a3 = a1.concat(a2);
 console.log(a1);
 console.log(a2);
 console.log(a3);
+
+let s1 = "hello";
+let s2 = "world";
+
+console.log(s1.concat(" ", s2, "!"));
+console.log(s1 + " " + s2 + "!");
