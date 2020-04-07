b2f213c10a7377e3cae7affb1b4c0478f26fe333
suminkim1007 <suminmusic99@gmail.com>
2020-04-07 12:18:16 +0900

    hw

--- hw/even.js
+++ hw/even.js
@@ -3,11 +3,11 @@ let a = [];
 for(let i = 0; i < 100; ++i){
     a[i] = Math.floor(Math.random() * 100 + 1);
 }
-console.log(a);
 
-for(let i = 99; i > -1; --i){
-    while(a[i] % 2 == 0)
-        a.pop();
+for(let i = 0; i < 100; ++i){
+    while(a[i] % 2 == 0){
+        a.splice(i, 1);
+    }
 }
 
 console.log(a);
