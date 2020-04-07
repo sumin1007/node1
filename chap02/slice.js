b9d98c59ecf76269f9c020806eaacf1a3cab2813
suminkim1007 <suminmusic99@gmail.com>
2020-04-07 10:08:27 +0900

    practice

--- chap02/slice.js
+++ chap02/slice.js
@@ -3,3 +3,8 @@ console.log(a.slice(0, 1));
 console.log(a.slice(0, 2));
 console.log(a.slice(1, 2));
 console.log(a.slice(1, 3));
+
+let s = "abcdefgh";
+console.log(s.slice(2, 4));
+console.log(s.slice(2));
+console.log(s.slice(-3));
