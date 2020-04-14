175c7a7209a4f67133bdbc569b82db5609828518
suminkim1007 <suminmusic99@gmail.com>
2020-04-14 15:16:54 +0900

    hw

---
+++ hw/personArray2.js
@@ -0,0 +1,14 @@
+let persons1 =  [ ];
+
+for(let i = 0; i < 10; i++){
+    persons1[i] = { name: "홍길동", age: 16 + i};
+}
+
+let persons2 = [ ];
+Object.assign(persons2, persons1);
+
+for(let i = 0; i < 10; i++){
+    persons2[i] = Object.assign({ }, persons1[i]);
+}
+
+console.log(persons2);
