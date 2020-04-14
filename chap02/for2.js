f436e272893d38c4d0aa6251f6300cac15411f25
suminkim1007 <suminmusic99@gmail.com>
2020-04-14 12:26:03 +0900

    practice

--- chap02/for2.js
+++ chap02/for2.js
@@ -3,3 +3,14 @@ for(let i = 0; i < s.length; ++i) {
     let ch = s[i]
     console.log(ch);
 }
+
+let rectangle = {
+    width: 5,
+    height: 7,
+    area : function() { return this.width * this.height; }
+};
+
+for (let key in rectangle){
+    let value = rectangle[key];
+    console.log("%s: %s", key, value);
+}
